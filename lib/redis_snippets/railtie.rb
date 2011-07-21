6ca9547405f5bdbdcde26498a6b264cb0995b5c6
Martin Moen Wulffeld <martin@wulffeld.org>
2011-07-21 14:04:05 +0200

    Only run the helper fix on <3.1.0.

--- lib/redis_snippets/railtie.rb
+++ lib/redis_snippets/railtie.rb
@@ -2,16 +2,18 @@ module RedisSnippets
   class Railtie < Rails::Railtie
     # This shouldn't be required when 3.1 arrives.
     # https://github.com/drogus/rails_helpers_fix
-    initializer "redis_snippets.include_railties_helpers_fix" do
-      helpers_paths = []
-      Rails.application.railties.all do |r|
-        if r.config.respond_to?(:paths) && r.config.paths.app.helpers
-          helpers_paths += r.config.paths.app.helpers.to_a
+    if Rails.version < "3.1.0"
+      initializer "redis_snippets.include_railties_helpers_fix" do
+        helpers_paths = []
+        Rails.application.railties.all do |r|
+          if r.config.respond_to?(:paths) && r.config.paths.app.helpers
+            helpers_paths += r.config.paths.app.helpers.to_a
+          end
         end
-      end
 
-      ActiveSupport.on_load(:action_controller) do
-        config.helpers_path += helpers_paths
+        ActiveSupport.on_load(:action_controller) do
+          config.helpers_path += helpers_paths
+        end
       end
     end
   end
